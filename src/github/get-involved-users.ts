ea7d894859f4a85e52e5159bf4ab3fc823b1e479
gentlementlegen <fernand.veyrier@epitech.eu>
2025-10-28 19:34:24 +0900

    fix: add GraphQL schema support and enhance involved user collection for linked pull requests

--- src/github/get-involved-users.ts
+++ src/github/get-involved-users.ts
@@ -1,4 +1,5 @@
 import { ContextPlugin } from "../types/index";
+import { PullRequest, Repository, User } from "@octokit/graphql-schema";
 
 export type InvolvedUser = {
   id: number;
@@ -7,6 +8,88 @@ export type InvolvedUser = {
 
 type IssueComment = Awaited<ReturnType<ContextPlugin["octokit"]["rest"]["issues"]["listComments"]>>["data"][number];
 type PullRequestReview = Awaited<ReturnType<ContextPlugin["octokit"]["rest"]["pulls"]["listReviews"]>>["data"][number];
+type PullRequestReviewComment = Awaited<ReturnType<ContextPlugin["octokit"]["rest"]["pulls"]["listReviewComments"]>>["data"][number];
+
+export const LINKED_PULL_REQUESTS = /* GraphQL */ `
+  query collectLinkedPullRequests($owner: String!, $repo: String!, $issue_number: Int!, $include_closed_prs: Boolean = false, $cursor: String) {
+    repository(owner: $owner, name: $repo) {
+      issue(number: $issue_number) {
+        id
+        closedByPullRequestsReferences(first: 10, includeClosedPrs: $include_closed_prs, after: $cursor) {
+          edges {
+            node {
+              id
+              title
+              number
+              url
+              state
+              author {
+                login
+                ... on User {
+                  id: databaseId
+                }
+              }
+              repository {
+                owner {
+                  login
+                }
+                name
+              }
+              labels(first: 100) {
+                nodes {
+                  id
+                  name
+                  description
+                }
+              }
+            }
+          }
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
+        }
+      }
+    }
+  }
+`;
+
+export type ClosedByPullRequestsReferences = {
+  node: Pick<PullRequest, "url" | "title" | "number" | "state" | "body"> & {
+    author: Pick<User, "login" | "id">;
+    repository: Pick<Repository, "owner" | "name">;
+    labels?: {
+      nodes: {
+        name: string;
+        description: string;
+      }[];
+    };
+  };
+};
+
+type IssueWithClosedByPrs = {
+  repository: {
+    issue: {
+      closedByPullRequestsReferences: {
+        edges: ClosedByPullRequestsReferences[];
+      };
+    };
+  };
+};
+
+export async function collectLinkedPulls(context: ContextPlugin, issue: { owner: string; repo: string; issue_number: number }, includeClosed: boolean = false) {
+  const { octokit } = context;
+  const { owner, repo, issue_number } = issue;
+
+  const result = await octokit.graphql.paginate<IssueWithClosedByPrs>(LINKED_PULL_REQUESTS, {
+    owner,
+    repo,
+    issue_number,
+    $include_closed_prs: includeClosed,
+  });
+
+  return result.repository.issue.closedByPullRequestsReferences.edges.map((edge) => edge.node);
+}
 
 export async function getInvolvedUsers(context: ContextPlugin<"issues.unassigned">): Promise<InvolvedUser[]> {
   const ownerLogin = context.payload.repository.owner?.login;
@@ -24,14 +107,29 @@ export async function getInvolvedUsers(context: ContextPlugin<"issues.unassigned
     per_page: 100,
   });
   collectParticipantsFromComments(participants, comments as IssueComment[]);
-  if ("pull_request" in context.payload.issue && context.payload.issue.pull_request) {
+  const linkedPullRequests = await collectLinkedPulls(context, { owner: ownerLogin, repo: repositoryName, issue_number: issueNumber });
+  for (const pullRequest of linkedPullRequests) {
+    const comments = await context.octokit.paginate(context.octokit.rest.issues.listComments, {
+      owner: ownerLogin,
+      repo: repositoryName,
+      issue_number: pullRequest.number,
+      per_page: 100,
+    });
+    collectParticipantsFromComments(participants, comments as IssueComment[]);
     const reviews = await context.octokit.paginate(context.octokit.rest.pulls.listReviews, {
       owner: ownerLogin,
       repo: repositoryName,
-      pull_number: issueNumber,
+      pull_number: pullRequest.number,
       per_page: 100,
     });
     collectParticipantsFromReviews(participants, reviews as PullRequestReview[]);
+    const reviewComments = await context.octokit.paginate(context.octokit.rest.pulls.listReviewComments, {
+      owner: ownerLogin,
+      repo: repositoryName,
+      pull_number: pullRequest.number,
+      per_page: 100,
+    });
+    collectParticipantsFromReviewComments(participants, reviewComments as PullRequestReviewComment[]);
   }
   return Array.from(participants.values());
 }
@@ -45,6 +143,12 @@ function collectParticipantsFromComments(store: Map<number, InvolvedUser>, comme
 function collectParticipantsFromReviews(store: Map<number, InvolvedUser>, reviews: PullRequestReview[]) {
   for (const review of reviews) {
     addParticipant(store, review?.user);
+  }
+}
+
+function collectParticipantsFromReviewComments(store: Map<number, InvolvedUser>, reviewComments: PullRequestReviewComment[]) {
+  for (const reviewComment of reviewComments) {
+    addParticipant(store, reviewComment?.user);
   }
 }
 
