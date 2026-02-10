570f0a3983c1ffed3a28579ef43c2bcdf9dfb242
gentlementlegen <fernand.veyrier@epitech.eu>
2026-02-10 16:41:20 +0900

    fix: sync workflow skipBotEvents and parameter metadata

--- src/types/command.ts
+++ src/types/command.ts
@@ -1,9 +1,12 @@
 import { StaticDecode, Type as T } from "@sinclair/typebox";
 
 export const xpCommandSchema = T.Object({
-  name: T.Literal("xp"),
+  name: T.Literal("xp", {
+    description: "Gets the XP for a user. If no user is specified, the user who invoked the command will be used.",
+    examples: ["/xp @username"],
+  }),
   parameters: T.Object({
-    username: T.Optional(T.String()),
+    username: T.Optional(T.String({ description: "GitHub username to get XP for.", examples: ["@UbiquityOS"] })),
   }),
 });
 
